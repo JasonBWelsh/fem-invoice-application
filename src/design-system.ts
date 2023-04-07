8a92ae915e5aa47188fb3b87c21e651f57dde3b3
Jason Welsh <jasonbwelsh@gmail.com>
2023-04-07 06:48:47 -0700

    add COLORS const in design system

---
+++ src/design-system.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+  mediumPurple: '#7C5DFA',
+  lightPurple: '#9277FF',
+  darkThree: '#1E2139',
+  darkCharcoal: '#252945',
+
+  lightPurpleGrey: '#DFE3FA',
+  mediumGrey: '#888EB0',
+  mediumBluePurple: '#7E88C3',
+  darkOne: '#0C0E16',
+
+  reddish: '#EC5757',
+  peach: '#9277FF',
+  lightBg: '#F8F8FB',
+  darkTwo: '#141625',
+};
+
+export const TYPOGRAPHY = {};
