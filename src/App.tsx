ff2c9417726a960d16d27edd23c129e33240f8ed
jason welsh <jasonbwelsh@gmail.com>
2023-08-09 18:12:52 -0700

    add test styled component to app for theme testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+// TEST temp import of `styled` for test - remove this
+import styled, {
+  ThemeProvider as StyledThemeProvider,
+} from 'styled-components';
 import { ThemeProvider, useTheme } from './state/context/theme-context';
 import { lightTheme, darkTheme } from './design-system/design-system';
 
 import './App.css';
+
+// TEST component for test - remove
+const TestBox = styled.div`
+  border: 3px solid var(--primary-color);
+  padding: 1rem 1.5rem;
+  background: var(--background-color);
+  color: var(--text-color);
+`;
+//
 
 const App = (): JSX.Element => {
   const { theme } = useTheme();
@@ -13,6 +25,8 @@ const App = (): JSX.Element => {
     <StyledThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <div className='App'>
         <div>Hi there!</div>
+
+        <TestBox>Here is some placeholder text to test the theme provider. Wow!!!</TestBox>
       </div>
     </StyledThemeProvider>
   );
